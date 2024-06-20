487f541cbee1691de94121cd95c6ea12484a41c7
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2024-06-21 00:55:35 +0200

    open3d

--- compares/rotated_cylinder.py
+++ compares/rotated_cylinder.py
@@ -10,45 +10,33 @@ radius = 4.0
 height = 10.0
 num_points = 1000
 
-# Generate random points on the surface of the cylinder
-theta = np.random.uniform(0, 2*np.pi, num_points)
-z = np.random.uniform(0, height, num_points)
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-# Combine the coordinates into a single array
-points = np.column_stack((x, y, z))
-
-# Calculate the normal vector pointing from the center to the surface points
-normals = np.column_stack((x, y, np.zeros(num_points)))
-normals /= np.linalg.norm(normals, axis=1)[:, np.newaxis]
-
-# rotation = random_rotation_matrix()
-
-# # Rotate the points and normals
-# points = np.dot(points, rotation.T)
-# normals = np.dot(normals, rotation.T)
+# Generate random rotation angles
+angles = np.random.uniform(0, 2*np.pi, 3)
+# mesh_cylinder = o3d.geometry.TriangleMesh.create_cylinder(radius=radius, height=height)
+mesh_cylinder = o3d.geometry.TriangleMesh.create_coordinate_frame()
+R = mesh_cylinder.get_rotation_matrix_from_xyz((np.pi / 2, 0, np.pi / 4))
+mesh_cylinder.rotate(R, center=(0, 0, 0))
+pcd_load = mesh_cylinder.sample_points_uniformly(number_of_points=num_points)
+# o3d.visualization.draw_geometries([pcd_load])
 
 min_noise = 0.05
 max_noise = 0.4
 noise_step = 0.05
 
-# Plot the 3D points
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(points[:, 0], points[:, 1], points[:, 2])
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-plt.show()
-
 for noise_scale in range(min_noise, max_noise, noise_step):
     # Add noise to the points position
-    x += np.random.normal(0, noise_scale, num_points)
-    y += np.random.normal(0, noise_scale, num_points)
-    z += np.random.normal(0, noise_scale, num_points)
+    pcd_load[:, 0] += np.random.normal(0, noise_scale, num_points)
+    pcd_load[:, 1] += np.random.normal(0, noise_scale, num_points)
+    pcd_load[:, 2] += np.random.normal(0, noise_scale, num_points)
 
-    center, axis, radius, inliers = pyransac3d.Cylinder().fit(points, thresh=0.2, maxIteration=1000)
-    data = USACFactory.cylinder_from_points_forming_circle().run(points)
+    # Plot the 3D points
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(pcd_load[:, 0], pcd_load[:, 1], pcd_load[:, 2])
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show()
 
-    points_with_normals = np.column_stack((points, normals))
+    center_pyransac3d, axis_pyransac3d, radius_pyransac3d, inliers = pyransac3d.Cylinder().fit(pcd_load, thresh=0.2, maxIteration=1000)
+    model, inliers, [center, radius, axis] = USACFactory.cylinder_from_points_forming_circle().run(pcd_load)
