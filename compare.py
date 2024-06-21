96c8a073b92668123f94ad60db8e90b7b88a49c5
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2024-06-21 23:39:44 +0200

    add compare files to compare entry

--- compare.py
+++ compare.py
@@ -1,13 +1,20 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Read named parameters')
-parser.add_argument('--type', type=str, help='name of comparison')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Read named parameters')
+    parser.add_argument('--type', type=str, help='name of comparison')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if args.type == None:
-    print('Error: --type must be specified.')
-    exit(1)
+    if args.type == None:
+        print('Error: --type must be specified.')
+        exit(1)
 
-if args.type == 'rotated_cylinder':
-    from compares.rotated_cylinder import *
+    if args.type == 'rotated_cylinder':
+        from compares.rotated_cylinder import *
+    
+    if args.type == 'rototranslated_cylinder':
+        from compares.rototranslated_cylinder import *
+    
+    if args.type == 'cylinder_with_other_object':
+        from compares.cylinder_with_other_object import *
